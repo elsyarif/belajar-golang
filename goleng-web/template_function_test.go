a46a0ebcbde8faeffc6070d3f874b5d753860e32
Syarif Hidayatulloh <id.syarif@gmail.com>
2022-10-25 05:53:22 +0700

    add file template function

---
+++ goleng-web/template_function_test.go
@@ -0,0 +1,76 @@
+package goleng_web
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type MyPage struct {
+	Name string
+}
+
+func (myPage MyPage) SayHello(name string) string {
+	return "Hello " + name + " My Name is " + myPage.Name
+}
+
+func TemplateFunction(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.New("FUNCTION").Parse(`{{.SayHello "Syarif"}}`))
+	t.ExecuteTemplate(writer, "FUNCTION", MyPage{
+		Name: "Budi",
+	})
+}
+
+func TestTemplateFunction(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:9000", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateFunction(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TemplateFunctionGlobal(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.New("FUNCTION").Parse(`{{len .Name}}`))
+	t.ExecuteTemplate(writer, "FUNCTION", MyPage{
+		Name: "Budi",
+	})
+}
+
+func TestTemplateFunctionGlobal(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:9000", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateFunctionGlobal(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TemplateFunctionMap(writer http.ResponseWriter, request *http.Request) {
+	t := template.New("FUNCTION")
+	t = t.Funcs(map[string]interface{}{
+		"upper": func(value string) string {
+			return strings.ToUpper(value)
+		},
+	})
+	t = template.Must(t.Parse(`{{ upper .Name}}`))
+	t.ExecuteTemplate(writer, "FUNCTION", MyPage{
+		Name: "Budi",
+	})
+}
+
+func TestTemplateFunctionMap(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:9000", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateFunctionMap(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
